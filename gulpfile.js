cbf57864c6181ffdda1c6be13802912923b15536
Sofie Wallin <sofie.wallin84@gmail.com>
2021-09-13 18:05:05 +0200

    Add tasks in gulp for concatenation, minimizing and mapping css and javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 const {src, dest, parallel, series, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps')
 
-/* Paths */
+// Define file paths
 const files = {
     htmlPath: 'src/**/*.html',
     cssPath: 'src/css/*.css',
@@ -9,32 +13,40 @@ const files = {
     imagesPath: 'src/images/*'
 }
 
-/* HTML-task */
+// HTML task
 function htmlTask() {
     return src(files.htmlPath)
     .pipe(dest('pub'));
 }
 
-/* CSS-task */
+// CSS task 
 function cssTask() {
     return src(files.cssPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.css'))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('../maps'))
     .pipe(dest('pub/css'))
     .pipe(browserSync.stream());
 }
 
-/* JavaScrip-task */
+// JavaScript task
 function jsTask() {
     return src(files.jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write('../maps'))
     .pipe(dest('pub/js'));
 }
 
-/* Images-task */
+// Images task
 function imagesTask() {
     return src(files.imagesPath)
     .pipe(dest('pub/images'));
 }
 
-/* Watch task */
+// Watch task
 function watchTask() {
     browserSync.init({
         server: "./pub"
@@ -42,6 +54,7 @@ function watchTask() {
     watch([files.htmlPath, files.cssPath, files.jsPath, files.imagesPath], parallel(htmlTask, cssTask, jsTask, imagesTask)).on('change', browserSync.reload);
 }
 
+// Export tasks
 exports.default = series(
     parallel(htmlTask, cssTask, jsTask, imagesTask),
     watchTask
